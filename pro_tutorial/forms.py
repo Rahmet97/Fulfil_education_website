8ea57477992d875aeb1f54774c77ba57450ed6ee
Jamshid <jamshid@dell.kali>
2020-10-19 00:26:59 +0500

    applying form for courses

--- pro_tutorial/forms.py
+++ pro_tutorial/forms.py
@@ -2,6 +2,33 @@ from django import forms
 
 
 class CourseForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    phone_number = forms.CharField(max_length=25)
-    email = forms.EmailField()
+    name         = forms.CharField(
+                   max_length=25,
+                   widget=forms.TextInput(
+                       attrs={
+                           "type": "text", 
+                           "placeholder": "FISH", 
+                           "class": "text",
+                       }
+                   )
+                )
+    phone_number = forms.CharField(
+                   max_length=25,
+                   widget=forms.TextInput(
+                       attrs={
+                           "type": "text", 
+                           "placeholder": "Telefon raqam", 
+                           "class": "tel",
+                       }
+                   )
+                )
+    email        = forms.EmailField(
+                   widget=forms.TextInput(
+                       attrs={
+                           "type": "email", 
+                           "placeholder": "Email",
+                           "name": "email",
+                           "class": "email",
+                       }
+                   )
+                )
