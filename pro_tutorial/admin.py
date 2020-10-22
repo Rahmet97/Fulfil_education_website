cdf0043aba8abea54e0e707e39e246ef73d17a95
Jamshid <jamshid@dell.kali>
2020-10-22 12:14:23 +0500

    configuring admin.py

--- pro_tutorial/admin.py
+++ pro_tutorial/admin.py
@@ -65,9 +65,9 @@ admin.site.register(ProCourseName, ProCourseNameAmin)
 
 class PupilAdmin(admin.ModelAdmin):
     list_display       = ('course_name', 'teacher_name', 'pupil_name', 'pupil_phonenumber', 'pupil_email', 'join_date', )
-    list_display_links = ('course_name', 'pupil_name', 'pupil_phonenumber', 'pupil_email', 'join_date', )
+    list_display_links = ('pupil_name', 'pupil_phonenumber', 'pupil_email', 'join_date', )
     search_fields      = ('pupil_name', 'teacher_name', 'course_name', )
-    list_editable      = ('teacher_name',)
+    list_editable      = ('course_name', 'teacher_name',)
     ordering           = ('pupil_name', 'pupil_phonenumber', 'pupil_email', 'join_date', 'teacher_name', 'course_name', )
 
     fieldsets = (
